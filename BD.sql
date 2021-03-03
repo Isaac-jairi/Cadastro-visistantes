9b13fdc1bb0103447b4dbcd8850b4627b3d20f2c
Isaac Jairi da Silva <isaacjairi12@gmil.com>
2021-03-03 15:03:04 -0400

    Adicionado novos modelos de avisos

--- BD.sql
+++ BD.sql
@@ -1,11 +1,21 @@
 CREATE DATABASE `controle_avisos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE TABLE `aniversariantes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomeAniversariante` varchar(45) DEFAULT NULL,
+  `dataAniversario` varchar(45) DEFAULT NULL,
+  `felicitador` varchar(45) DEFAULT NULL,
+  `observacao` varchar(1000) DEFAULT NULL,
+  `apresentado` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `avisos_gerais` (
   `id` int NOT NULL AUTO_INCREMENT,
   `aviso` varchar(1000) DEFAULT NULL,
   `apresentado` tinyint DEFAULT '0',
   `avisador` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `carta_mudanca` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -16,7 +26,36 @@ CREATE TABLE `carta_mudanca` (
   `tipoarquivo` varchar(255) NOT NULL,
   `apresentado` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `carta_recomendacao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `procedencia` varchar(255) DEFAULT NULL,
+  `nomearquivo` varchar(255) NOT NULL,
+  `tamanhoarquivo` int NOT NULL,
+  `tipoarquivo` varchar(255) NOT NULL,
+  `apresentado` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `pedido_oportunidade` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomePedinte` varchar(45) DEFAULT NULL,
+  `oportunidade` varchar(100) DEFAULT NULL,
+  `observacoes` varchar(100) DEFAULT NULL,
+  `apresentado` tinyint DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `pedido_oracao` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nomePedinte` varchar(100) DEFAULT NULL,
+  `nomeFavorecido` varchar(100) DEFAULT NULL,
+  `apresentado` tinyint DEFAULT '0',
+  `observacoes` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `visitantes` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -25,12 +64,4 @@ CREATE TABLE `visitantes` (
   `apresentado` tinyint DEFAULT '0',
   `convidante` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `controle_avisos`.`pedido_oracao` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nomePedinte` VARCHAR(100) NULL,
-  `nomeFavorecido` VARCHAR(100) NULL,
-  `apresentado` TINYINT NULL DEFAULT '0',
-  `observacoes` VARCHAR(1000) NULL,
-  PRIMARY KEY (`id`))DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
